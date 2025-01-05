6a10452f61caf8adf71c9993c4568250fcaa27c3
rock4ts <rock4ts@gmail.com>
2025-01-05 04:46:26 +0300

    В трейсер добавлен тэг request_id

--- src/main.py
+++ src/main.py
@@ -72,14 +72,19 @@ def authjwt_exception_handler(request: Request, exc: AuthJWTException):
 async def before_request(request: Request, call_next):
     client_ip = request.headers.get('X-Real-IP', '').lower()
     is_local = client_ip in {'localhost', '127.0.0.1', '', None}
-    request_id = request.headers.get('X-Request-Id')
+    request_id = request.headers.get('X-Request-Id', 'unknown')
 
     # Не используем трассировкy локальных запросов
     if not is_local and not request_id:
-        return ORJSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={'detail': 'X-Request-Id is required'})
-
-    response = await call_next(request)
-    return response 
+        return ORJSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={'detail': 'X-Request-Id is required'}
+        )
+    
+    tracer = trace.get_tracer(__name__)
+    with tracer.start_as_current_span(f"{request.method} {request.url.path}") as span:
+        span.set_attribute("http.request_id", request_id)
+        return await call_next(request)
 
 
 if __name__ == "__main__":
